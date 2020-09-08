486c75a6c3507bc9f896d4f36bd821d5bde9d8ed
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-08 09:32:52 -0500

    added prompt questions

--- app.js
+++ app.js
@@ -10,7 +10,31 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name:"
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter your email address:"
+          },
+          {
+            type: "input",
+            name: "role",
+            message: "Select your role in the company:",
+            choices: ['Employee', 'Engineer', 'Manager', 'Intern']
+          },
+          {
+            type: "input",
+            name: "usage",
+            message: "Please provide instructions & examples for this application in use:"
+          } 
+    ])
+}
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
