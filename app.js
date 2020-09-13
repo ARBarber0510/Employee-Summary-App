0132e0e0d4d06b2b7aac18e9cf3a5abd32544a05
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-13 17:37:50 -0500

    attempted to add second function to restart prompt to add more employees.

--- app.js
+++ app.js
@@ -63,11 +63,12 @@ const engineerQuestion = [
 ]
 
 // Additional employees question
-const addEmployee = [
+const moreEmpQuestion = [
     {
-        type: "confirm",
+        type: "list",
         name: "addEmployees",
-        message: "Would you like to add more employees?"
+        message: "Would you like to add more employees?",
+        choices: ["Yes", "No"]
         }
 ]
 
@@ -87,12 +88,14 @@ async function promptUser() {
                     const answer = await inquirer.prompt(employeeQuestions)
                     
                     // Run switch case based on the answers provided for each employee
+                    // Pushing answers object into into addEmployeeObject
                     switch(answer.role){
                         
                         case "Manager": {
                             const managerAnswer = await inquirer.prompt(managerQuestion);
                             const newManager = new Manager(answer.name, answer.id, answer.email, managerAnswer.officeNumber)
                             employees.push(newManager);
+                            restartPrompt();
                             break;
                         };
                          
@@ -100,6 +103,7 @@ async function promptUser() {
                             const internAnswer = await inquirer.prompt(internQuestion);
                             const newIntern = new Intern(answer.name, answer.id, answer.email, internAnswer.schoolName)
                             employees.push(newIntern);
+                            restartPrompt();
                             break;    
                         };
                     
@@ -107,12 +111,14 @@ async function promptUser() {
                             const engineerAnswer = await inquirer.prompt(engineerQuestion);
                             const newEngineer = new Engineer(answer.name, answer.id, answer.email, engineerAnswer.githubName)
                             employees.push(newEngineer);
+                            restartPrompt();
                             break;
                         }
                     }
-                    const addEmployeeObject = await inquirer.prompt(addEmployee);
+                    const addEmployeeObject = await inquirer.prompt(moreEmpQuestion);
 
                     addEmployees = addEmployeeObject.more;
+
                 };     
                     
                 return employees;
@@ -120,10 +126,25 @@ async function promptUser() {
 
         catch(err) {
             console.log(err);
-        }
+        };
+    };
+
+    // Attempted to restart the prompt to add more employees
+    function restartPrompt() {
+        inquirer.prompt(employeeQuestions.moreEmpQuestion).then(answer => {
+            switch (answer.role) {
+                case "Yes":
+                    promptUser();
+                    break;
+    
+                case "No":
+                    buildHTML();
+                    break;
+            }
+        });
     }
 
-
+    // Build team.html based off the information provided.
     async function buildHTML() {
 
 
@@ -136,30 +157,10 @@ async function promptUser() {
                 return console.log(err);
             }
             else {
-                console.log("HTML Generated");
+                console.log("Successfully generated team.html");
             }
         });
     }
     
     buildHTML();
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
